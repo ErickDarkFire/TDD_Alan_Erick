5d2c4f3f763635cefaae547907854d736918fa1f
alan80avalos <alejando.rodriguez@iteso.mx>
2026-04-01 12:04:18 -0600

    fix: updated exercise1 fizzbuzz for multiples of five

--- exercise1.py
+++ exercise1.py
@@ -5,6 +5,8 @@ def fizzBuzz(num):
     try:
         if num % 3 == 0:
             return "Fizz"
+        elif num % 5 == 0:
+            return "Buzz"
         elif num == int(num):
             return str(num)
     except:
