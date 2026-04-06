7376c064913486dded3e8f0a670e7459635a8a29
alan80avalos <alejando.rodriguez@iteso.mx>
2026-04-06 12:54:34 -0600

    fix: add the req for a password must contain at least 2 numbers

--- exercise3.py
+++ exercise3.py
@@ -7,6 +7,15 @@ def passwordValidation(password):
     if(len(password) < 8):
         returns.append("Pasword must be at least 8 characters")
     
+    digit_count = 0
+
+    for i in password:
+        if i.isdigit():
+            digit_count +=1
+
+    if(digit_count < 2):
+        returns.append("The passwordd must contain at least 2 numbers")
+        
     return {
         "isValid": len(returns) == 0,
         "errors": returns
