adf192d288e9d9699023dff6be6eb78b08eb8832
Erick <erick.rodriguez@iteso.mx>
2026-04-06 11:36:07 -0600

    fix: add method handle an unknown number of arguments

--- exercise2.py
+++ exercise2.py
@@ -1,11 +1,15 @@
 """
 Exercise2
 """
+
 def Add(nums):
     list_nums = nums.split(",")
-    if len(list_nums) == 1 and list_nums[0] == "":
+    if nums == "":
         return 0
     if len(list_nums) == 1:
         return int(list_nums[0])
-    if len(list_nums) == 2:
-        return int(list_nums[0]) + int(list_nums[1])
+    else:
+        acum = 0 
+        for i in range(0,len(list_nums)):
+            acum += int(list_nums[i])
+        return acum
