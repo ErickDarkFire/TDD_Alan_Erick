4fd663dc81e90193a900890c62b3440deba9e7cf
Erick <erick.rodriguez@iteso.mx>
2026-04-06 11:31:10 -0600

    fix: fizzbuzz function updated for multiples of three and five

--- exercise1.py
+++ exercise1.py
@@ -3,7 +3,9 @@ Exercise1
 """
 def fizzBuzz(num):
     try:
-        if num % 3 == 0:
+        if num % 3 == 0 and num % 5 == 0:
+            return "FizzBuzz"
+        elif num % 3 == 0:
             return "Fizz"
         elif num % 5 == 0:
             return "Buzz"
