339eb57fd77ef8786fa6618db56b158ade795fed
alan80avalos <alejando.rodriguez@iteso.mx>
2026-04-06 11:37:07 -0600

    feat: add function updated to handle newlines as separators

--- exercise2.py
+++ exercise2.py
@@ -1,15 +1,16 @@
 """
 Exercise2
 """
+import re
 
 def Add(nums):
-    list_nums = nums.split(",")
-    if nums == "":
+    list_nums = re.split(r'[,\n]+',nums)
+    if len(list_nums) == 1 and list_nums[0] == "":
         return 0
-    if len(list_nums) == 1:
+    elif len(list_nums) == 1:
         return int(list_nums[0])
     else:
-        acum = 0 
+        acum = 0
         for i in range(0,len(list_nums)):
             acum += int(list_nums[i])
         return acum
