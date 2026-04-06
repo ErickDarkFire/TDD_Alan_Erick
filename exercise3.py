795a5c5c0705f917f8dfa0543d57f0ec281ab9c5
Erick <erick.rodriguez@iteso.mx>
2026-04-06 12:57:10 -0600

    fix: add the req for a password must contain at least one capital letter

--- exercise3.py
+++ exercise3.py
@@ -8,14 +8,20 @@ def passwordValidation(password):
         returns.append("Pasword must be at least 8 characters")
     
     digit_count = 0
+    capital_count = 0
 
     for i in password:
         if i.isdigit():
             digit_count +=1
+        elif i.isupper():
+            capital_count +=1
 
     if(digit_count < 2):
         returns.append("The passwordd must contain at least 2 numbers")
-        
+
+    if(capital_count < 1):
+        returns.append("password must contain at least one capital letter")
+
     return {
         "isValid": len(returns) == 0,
         "errors": returns
