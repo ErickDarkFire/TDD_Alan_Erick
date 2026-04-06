0fb9394bab64e25217b7950ec302311e0eabc40a
Erick <erick.rodriguez@iteso.mx>
2026-04-06 11:38:57 -0600

    fix: add method not allow separator at the end

--- exercise2.py
+++ exercise2.py
@@ -4,13 +4,19 @@ Exercise2
 import re
 
 def Add(nums):
-    list_nums = re.split(r'[,\n]+',nums)
-    if len(list_nums) == 1 and list_nums[0] == "":
+    if nums == "":
         return 0
-    elif len(list_nums) == 1:
+    
+    sep = [',','\n']
+    size = len(nums)
+    if nums[size-1] in sep:
+        raise ValueError("Invalido")
+    
+    list_nums = re.split(r'[,\n]+',nums)
+    if size == 1:
         return int(list_nums[0])
     else:
-        acum = 0
+        acum = 0 
         for i in range(0,len(list_nums)):
             acum += int(list_nums[i])
         return acum
