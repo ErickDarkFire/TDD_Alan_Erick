f5ab52501c952d6db4c11ecd365a90a5e99a67f4
Erick <erick.rodriguez@iteso.mx>
2026-04-15 01:20:47 -0600

    feat: finished with exercise 6

--- kata6/test_banking.py
+++ kata6/test_banking.py
@@ -5,7 +5,7 @@ Erick Eduardo Rodriguez Gomez
 Exercise6
 """
 
-from kata6.exercise6 import Banking
+from exercise6 import Banking
 import unittest
 from pathlib import Path
 from io import StringIO
@@ -14,6 +14,7 @@ from unittest.mock import patch
 from datetime import datetime as real_datetime
 
 class TestBacking(unittest.TestCase):
+    
     def setUp(self):
         self.ruta = Path("kata6/table.txt")
         self.ruta.write_text("")
@@ -29,38 +30,38 @@ class TestBacking(unittest.TestCase):
 
     @patch("kata6.exercise6.datetime")
     def test_ShouldRegisterADeposit_WhenDepositIsMade(self, mock_datetime):
-        mock_datetime.now.return_value = real_datetime(2014, 4, 1)
+        mock_datetime.now.return_value = real_datetime(2026, 4, 15)
 
         Banking.deposit(1000)
 
         self.assertEqual(
             self.ruta.read_text(),
-            "01/04/2014,1000.00,1000.00\n"
+            "15/04/2026,1000.00,1000.00\n"
         )
 
     @patch("kata6.exercise6.datetime")
     def test_ShouldRegisterAWithdraw_WhenWithdrawIsMade(self, mock_datetime):
-        mock_datetime.now.return_value = real_datetime(2014, 4, 1)
+        mock_datetime.now.return_value = real_datetime(2026, 4, 15)
         Banking.deposit(1000)
 
-        mock_datetime.now.return_value = real_datetime(2014, 4, 2)
+        mock_datetime.now.return_value = real_datetime(2026, 4, 15)
         Banking.withdraw(100)
 
         self.assertEqual(
             self.ruta.read_text(),
-            "01/04/2014,1000.00,1000.00\n"
-            "02/04/2014,-100.00,900.00\n"
+            "15/04/2026,1000.00,1000.00\n"
+            "15/04/2026,-100.00,900.00\n"
         )
 
     @patch("kata6.exercise6.datetime")
     def test_ShouldPrintStatement_WhenThereAreMovements(self, mock_datetime):
-        mock_datetime.now.return_value = real_datetime(2014, 4, 1)
+        mock_datetime.now.return_value = real_datetime(2026, 4, 15)
         Banking.deposit(1000)
 
-        mock_datetime.now.return_value = real_datetime(2014, 4, 2)
+        mock_datetime.now.return_value = real_datetime(2026, 4, 15)
         Banking.withdraw(100)
 
-        mock_datetime.now.return_value = real_datetime(2014, 4, 10)
+        mock_datetime.now.return_value = real_datetime(2026, 4, 15)
         Banking.deposit(500)
 
         with patch("sys.stdout", new_callable=StringIO) as fake_out:
@@ -69,9 +70,9 @@ class TestBacking(unittest.TestCase):
 
         expected_output = (
             "DATE|AMOUNT|BALANCE\n"
-            "10/04/2014|500.00|1400.00\n"
-            "02/04/2014|-100.00|900.00\n"
-            "01/04/2014|1000.00|1000.00"
+            "15/04/2026|500.00|1400.00\n"
+            "15/04/2026|-100.00|900.00\n"
+            "15/04/2026|1000.00|1000.00"
         )
 
         self.assertEqual(
